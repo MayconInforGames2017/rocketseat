f3ad401bf0c5b35bb4dc73459c787f25a1a5df52
maycon santana <maycon_gus2009@hotmail.com>
2021-08-11 19:02:29 -0300

    criando a conexao com o banco sqlite

--- node js/nlwValoriza/src/server.ts
+++ node js/nlwValoriza/src/server.ts
@@ -1,26 +1,6 @@
-import express, { request, response } from "express";
+import "reflect-metadata";  
+import express from "express";
 
-// @Types/express
 const app = express();
 
-/**  
- * GET    => Buscar uma informação
- * POST   => Inserir (Criar) uma informação
- * PUT    => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH  => Alterar uma informação específica
-*/
-
-// criando rotas
-app.get("/test", (request, response) => {
-    // Request => Entrando
-    // Response => Saindo
-    return response.send("Olá NLW"); 
-});
-
-app.post("/test-post", (request, response) => {
-    return response.send("Olá NLW método POST");
-});
-
-// http://localhost:3000
 app.listen(3000, () => console.log("Servido está funcionando"));
