5555a91f1614221a7630412c810bae07ce35ddfa
maycon santana <maycon_gus2009@hotmail.com>
2021-08-11 16:28:38 -0300

    conhecendo os tipos de metodos e criando a primeira rota

--- node js/nlwValoriza/src/server.ts
+++ node js/nlwValoriza/src/server.ts
@@ -1,7 +1,22 @@
-import express from "express";
+import express, { request, response } from "express";
 
 // @Types/express
 const app = express();
+
+/**  
+ * GET    => Buscar uma informação
+ * POST   => Inserir (Criar) uma informação
+ * PUT    => Alterar uma informação
+ * DELETE => Remover um dado
+ * PATCH  => Alterar uma informação específica
+*/
+
+// criando rotas
+app.get("/test", (request, response) => {
+    // Request => Entrando
+    // Response => Saindo
+    return response.send("Olá NLW"); 
+});
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Servido está funcionando"));
