38e804ede94321fd8dfc89225ff9a12845c64eda
maycon santana <maycon_gus2009@hotmail.com>
2021-08-12 16:56:07 -0300

    criando a tabela users

---
+++ node js/nlwValoriza/src/database/migrations/1628797568479-CreateUsers.ts
@@ -0,0 +1,47 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsers1628797568479 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid", // id universal, que raramente irá se repetir
+                        isPrimary: true, 
+                        //generationStrategy: "uuid" 
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar"
+                    },
+                    {
+                        name: "admin",
+                        type: "boolean",
+                        default: false
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
